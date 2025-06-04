5c42691ee463c6023610ac813b6ba241ecad002c
Diego <diegoavieira@gmail.com>
2025-06-04 15:58:08 -0300

    Adicionado .env.example

--- eslint.config.js
+++ eslint.config.js
@@ -21,8 +21,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': 'error'
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
     }
   }
 );
